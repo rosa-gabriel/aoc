868e408ca054f67d45077b829f1d9a4bdbec4ef3
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2023-08-06 18:06:27 -0300

    2022 02

--- 2022/n02/src/main.rs
+++ 2022/n02/src/main.rs
@@ -1,18 +1,42 @@
 use std::fs;
 
+struct Round {
+    player_sign: u8,
+    oponent_sign: u8
+}
+
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap(); 
-
-    let result = file
+    let result: i32 = file
         .split("\n")
         .map(|item| {
-            let arr = item
-                .split(" ")
-                .collect::<Vec<&str>>();
+                let options: Vec<&str> = item.split(" ").collect();
+                if item.len() < 2 { return 0 };
 
-            let a = arr[0];
-            let b = arr[1];
-        }).sum::<i32>();
+                let round = Round { 
+                    player_sign: options[1].as_bytes()[0],
+                    oponent_sign: options[0].as_bytes()[0]
+                };
+                return check_round_result(round);
+        })
+        .sum();
 
-    println!("{}", result);
+    println!("{:?}", result);
 }
+
+fn check_round_result(round: Round) -> i32 {
+    let player_sign_value: u8 = round.player_sign - 87;
+    let oponent_sign_value: u8 = round.oponent_sign - 64;
+
+    let mut battle_result_value: u8 = 0;
+
+    if player_sign_value == oponent_sign_value {
+        battle_result_value = 3;
+    } else if 
+        (player_sign_value > oponent_sign_value && !(player_sign_value == 3 && oponent_sign_value == 1)) || 
+        (player_sign_value == 1 && oponent_sign_value == 3){
+
+        battle_result_value = 6;
+    } 
+    return (player_sign_value + battle_result_value) as i32;
+} 
